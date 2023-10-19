fc94dad565631b908bce0210546fe1079d08c7ec
Landiel <129320343+LandielDurans@users.noreply.github.com>
2023-10-18 22:10:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function clock() {
+    var date = new Date()
+    var hour = date.getHours()
+    var min = date.getMinutes()
+    var sec = date.getSeconds()
+    var msg = document.getElementById('res')
+
+    hour = (hour < 10) ? '0' + hour : hour
+    min = (min < 10) ? '0' + min : min
+    sec = (sec < 10) ? '0' + sec : sec
+    
+    if (hour <= 12){
+        res.innerText = 'Bom dia.'
+    }
+    else if (hour <= 18){
+        res.innerText = 'Boa tarde'
+    }
+    else{
+        res.innerText = 'Boa noite'
+    }
+    
+    var time = `${hour}:${min}:${sec}`
+    document.getElementById('myclock').innerText = time
+    setTimeout(clock, 1000)
+}
+
+clock();
+
+var toggle = document.getElementById('toggle_checkbox')
+
+function darkmode(){
+    var bg = document.body
+    bg.classList.toggle('lightmode')
+    bg.style.transition = '0.5s ease all'
+}
